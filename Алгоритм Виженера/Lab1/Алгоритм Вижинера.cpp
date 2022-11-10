4576de4ada185fc530840c8f67b80511e4b05386
DmitryZab <73656791+DmitryZab@users.noreply.github.com>
2022-11-10 15:56:36 +0400

    Add files via upload

---
+++ Алгоритм Виженера/Lab1/Алгоритм Вижинера.cpp
@@ -0,0 +1,54 @@
+﻿
+// Алгоритм Вижинера
+
+#include <iostream>
+#include <string>
+using namespace std;
+class Vig {
+public:
+    string k;
+    Vig(string k) {
+        for (int i = 0; i < k.size(); ++i) {
+            if (k[i] >= 'A' && k[i] <= 'Z')
+                this->k += k[i];
+            else if (k[i] >= 'a' && k[i] <= 'z')
+                this->k += k[i] + 'A' - 'a';
+        }
+    }
+    string encryption(string t) {
+        string output;
+        for (int i = 0, j = 0; i < t.length(); ++i) {
+            char c = t[i];
+            if (c >= 'a' && c <= 'z')
+                c += 'A' - 'a';
+            else if (c < 'A' || c > 'Z')
+                continue;
+            output += (c + k[j] - 2 * 'A') % 26 + 'A'; 
+            j = (j + 1) % k.length();
+        }
+        return output;
+    }
+    string decryption(string t) {
+        string output;
+        for (int i = 0, j = 0; i < t.length(); ++i) {
+            char c = t[i];
+            if (c >= 'a' && c <= 'z')
+                c += 'A' - 'a';
+            else if (c < 'A' || c > 'Z')
+                continue;
+            output += (c - k[j] + 26) % 26 + 'A';
+            j = (j + 1) % k.length();
+        }
+        return output;
+    }
+};
+int main() {
+    setlocale(LC_ALL, "RUS");
+    Vig v("ASWEWER");
+    string ori = "PRIVET";
+    string encrypt = v.encryption(ori);
+    string decrypt = v.decryption(encrypt);
+    cout << "Оригинальный текст: " << ori << endl;
+    cout << "Зашиврованный текст: " << encrypt << endl;
+    cout << "Расшифрованный текст: " << decrypt << endl;
+}
